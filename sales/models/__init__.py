3c05149d9f725a09837ef6ad9cd7657ea47e3b0f
celelstine <okorocelestine@gmail.com>
2019-12-10 22:00:12 +0100

    use snake case for file names

--- sales/models/__init__.py
+++ sales/models/__init__.py
@@ -1,13 +1,13 @@
 
-from .airtimeRecieved import AirtimeRecieved
+from .airtime_recieved import AirtimeRecieved
 from .configuration import Configuration
 from .dataplan import DataPlan
 from .data_sales import DataSales
 from .data_sales_diary_summary import DataSalesSummary
-from .dataSubscription import DataSubscription
+from .data_subscription import DataSubscription
 from .product import Product
 from .salesrep import SalesRep
-from .salesrepDataSubscription import SalesRepDataSubscription
+from .salesrep_data_subscription import SalesRepDataSubscription
 
 __all__ = [
     'AirtimeRecieved',
