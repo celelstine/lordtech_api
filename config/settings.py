da9ebcd4d769f017eb0c3b1422cd2f7f8129f690
celelstine <okorocelestine@gmail.com>
2020-02-05 12:57:55 +0100

    handle cases where debug setting is non-boolean

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,9 @@ if os.getenv('HOST_ENV', 'Dev') != 'HEROKU':
 SECRET_KEY = os.getenv('SECRET_KEY', 'badguy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.getenv('DEBUG', '1')))
+DEBUG = os.getenv('DEBUG', False)
+if not isinstance(DEBUG, bool):
+    DEBUG = bool(int(DEBUG))
 
 AUTH_USER_MODEL = 'LtSales_auth.User'
 
