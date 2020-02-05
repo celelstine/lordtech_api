7b732bc03cb48c452649b6946e7df22f75699348
celelstine <okorocelestine@gmail.com>
2020-02-05 12:58:41 +0100

    add user role and is_Admin to user serializer

--- api/v1/serializers/auth.py
+++ api/v1/serializers/auth.py
@@ -11,10 +11,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'id', 'username', 'email', 'first_name',
-            'last_name',
+            'last_name', 'role', 'is_admin'
         )
         extra_kwargs = {
             'username': {
                 'allow_null': False, 'required': True, 'allow_blank': False
             },
         }
+        read_only_fields = ['role', 'is_admin']
+
+    is_admin = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()
+
+    def get_role(self, user):
+        return user.get_role_display()
+
+    def get_is_admin(self, user):
+        return user.is_superuser
